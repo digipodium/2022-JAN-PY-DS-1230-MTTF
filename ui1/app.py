7fcc5ca68740cf2d9fd8a9411fed5a43d14f510e
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-08 14:14:04 +0530

    flask form

--- ui1/app.py
+++ ui1/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         print(request.form)
-    return render_template('index.html')``
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
