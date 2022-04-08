3d9782323b66623a5df21bbac5089a85cd8a358e
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-08 14:09:16 +0530

    form design

--- ui1/app.py
+++ ui1/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('index.html')``
 
 @app.route('/about')
 def about():
