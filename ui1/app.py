d07de925a213a10b97c2de1f45462ccb30f95ad3
zaid kamil <xaidmetamorphos@gmail.com>
2022-04-12 13:21:50 +0530

    form submission

--- ui1/app.py
+++ ui1/app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 app = Flask(__name__)
+app.secret_key = 'thisisaverysecretkey'
 
 
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
+        question = request.form.get('question')
+        op1  = request.form.get('op1')
+        op2  = request.form.get('op2')
+        op3  = request.form.get('op3')
+        op4  = request.form.get('op4')
+        ans = request.form.get('ans')
+        category = request.form.get('category')
+        if not question or len(question) < 10:
+            flash('Question is required and should be at least 10 characters long', 'danger')
+            return redirect('/')
+        elif not op1:
+            flash('Option 1 is required', 'danger')
+            return redirect('/')
+        # more like this
+        else:
+            flash('Question added successfully', 'success')
+            return redirect('/')
+
     return render_template('index.html')
 
 @app.route('/about')
